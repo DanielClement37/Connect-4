b7ac532ade0d24da49fd7e15848854c3c85b55ea
Daniel <Daniel.clement37@gmail.com>
2024-04-12 23:25:21 -0500

    Setup main menu html with little styling

--- src/Pages/MainMenu.tsx
+++ src/Pages/MainMenu.tsx
@@ -1,20 +1,18 @@
 import { Link } from "react-router-dom";
 import GameLogo from "../assets/images/logo.svg";
 import PvpImg from "../assets/images/player-vs-player.svg";
-import "../Styles/mainMenu.css"
+import "../Styles/mainMenu.css";
 
 export const MainMenu = () => {
 	return (
-		<div className="menu-card">
-			<img src={GameLogo} alt="game logo" />
-			<div className="pvp-btn">
-				<Link to={`Game`}>
+		<div className="menu-page">
+			<div className="menu-card">
+				<img src={GameLogo} alt="game logo" />
+				<Link to={`Game`} className="pvp menu-btn link">
 					<h2 className="heaing-m">PLAY VS PLAYER</h2>
 					<img src={PvpImg} alt="two faces" />
 				</Link>
-			</div>
-			<div className="rules-btn">
-				<Link to={`Rules`}>
+				<Link to={`Rules`} className="rules menu-btn link">
 					<h2 className="heaing-m">GAME RULES</h2>
 				</Link>
 			</div>
