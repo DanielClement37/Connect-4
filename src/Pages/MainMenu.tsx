499a89c8e6bd0f60595226c764a28b156ed5fce5
Daniel <Daniel.clement37@gmail.com>
2024-04-13 22:50:52 -0500

    made main menu mobile friendly

--- src/Pages/MainMenu.tsx
+++ src/Pages/MainMenu.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import GameLogo from "../assets/images/logo.svg";
 import PvpImg from "../assets/images/player-vs-player.svg";
-import "../Styles/mainMenu.css";
+import "../Styles/mainMenu/mainMenuDesk.css"
+import "../Styles/mainMenu/mainMenuMobile.css"
 
 export const MainMenu = () => {
 	return (
 		<div className="menu-page">
 			<div className="menu-card">
-				<img src={GameLogo} alt="game logo" />
+				<img className="menu-img" src={GameLogo} alt="game logo" />
 				<Link to={`Game`} className="pvp menu-btn link">
 					<h3 className="heaing-m">PLAY VS PLAYER</h3>
 					<img src={PvpImg} alt="two faces" />
