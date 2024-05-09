de84cf6da9f51eed8111e92ddd9c561ff6ec6911
Daniel <Daniel.clement37@gmail.com>
2024-05-08 22:00:45 -0500

    Marker displays on desktop and follows cursor

--- src/Components/GameBoard.tsx
+++ src/Components/GameBoard.tsx
@@ -1,22 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Cell } from "../Types/GameTypes";
 import "../Styles/gameStyles/gameBoardDesk.css";
+import { useContext, useState } from "react";
+import { AppContext } from "../GlobalState/Context/AppContext";
+import { classNames } from "../Utils/ClassNames";
+import { Player } from "../Types/Enums";
 
 export interface GameBoardProps {
 	gameBoard: Cell[][];
 }
 
 export const GameBoard = ({ gameBoard }: GameBoardProps) => {
+	const { state, dispatch } = useContext(AppContext);
+	const {gameState} = state
+	
+	const [hoveredCol, setHoveredCol] = useState<number>(0);
+	const columnWidth = 632 / 7; // Calculate the width of each column
+	const markerWidth = 38; // The width of the marker
+    const adjustment = (columnWidth - markerWidth) / 2;
+
 	return (
-			<div className="board-container">
-				<div className="board-grid">
-					{gameBoard.map((col, colIndex) => (
-						<div key={colIndex} className="board-col">
-							{col.map((cell, rowIndex) => (
-								<div key={rowIndex} className="board-cell"></div>
-							))}
-						</div>
-					))}
-				</div>
+		<div className="board-container">
+			<div className="marker-container">
+				<div
+					className={classNames(gameState.currPlayer === Player.RED ? "marker-red" : "marker-yellow")}
+					style={{
+						left: hoveredCol !== null ? `${hoveredCol * columnWidth + adjustment}px` : "0px",
+						position: "absolute",
+						transition: "left 0.1s ease",
+					}}
+				/>
 			</div>
+			<div className="board-grid">
+				{gameBoard.map((col, colIndex) => (
+					<div key={colIndex} className="board-col" onMouseEnter={() => {setHoveredCol(colIndex); console.log(colIndex)}}>
+						{col.map((cell, rowIndex) => (
+							<div key={rowIndex} className="board-cell"></div>
+						))}
+					</div>
+				))}
+			</div>
+		</div>
 	);
 };
